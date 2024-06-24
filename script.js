38cecb8b281a2d87f66134ad6406fd0971d049de
Alisson Araújo <alissonvinicios2@gmail.com>
2024-06-24 09:42:09 -0300

    Guardando os valores de usuário

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const btnLogar = document.querySelector('#buttonLogin');
+const userLogin = document.querySelector('#userLogin').value;
+const userSenha = document.querySelector('#userSenha').value;
+
 
 btnLogar.addEventListener('click', () =>{
   console.log('confimo')
