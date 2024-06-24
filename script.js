95df51613dd105b040489a97147d202090726077
Alisson Araújo <alissonvinicios2@gmail.com>
2024-06-24 17:18:40 -0300

    Ajeitando como recebe os dados

--- script.js
+++ script.js
@@ -2,21 +2,10 @@ const btnLogar = document.querySelector('#buttonLogin');
 const userLogin = document.querySelector('#userLogin');
 const userSenha = document.querySelector('#userSenha');
 
-
-btnLogar.addEventListener('click', () =>{
-  var loginUserAtual = userLogin.value;
-  var senhaUserAtual = userSenha.value;
-
-  fetch('https://dummyjson.com/user/login', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      
-      username: loginUserAtual,
-      password: senhaUserAtual,
-      expiresInMins: 30, // optional, defaults to 60
-    })
-  })
-  .then(res => res.json())
-  .then(console.log);
-})
+let coletaDados = function() {
+  let usuario = {
+    user : document.querySelector('#userLogin').value,
+    senha : document.querySelector('#userSenha').value
+  }
+  return usuario
+}
