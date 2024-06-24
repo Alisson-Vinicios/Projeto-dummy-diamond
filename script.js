7cfa5bdc3ddffeb31c3e3c03c6bb66426d88c934
Alisson Araújo <alissonvinicios2@gmail.com>
2024-06-24 09:55:44 -0300

    Puxando usuário
    Pegando a variável escrita pelo usuário e  puxando os dados do dummy

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const btnLogar = document.querySelector('#buttonLogin');
-const userLogin = document.querySelector('#userLogin').value;
-const userSenha = document.querySelector('#userSenha').value;
+const userLogin = document.querySelector('#userLogin');
+const userSenha = document.querySelector('#userSenha');
 
 
 btnLogar.addEventListener('click', () =>{
-  console.log('confimo')
-  return;
+  var loginUserAtual = userLogin.value;
+  var senhaUserAtual = userSenha.value;
+
+  fetch('https://dummyjson.com/user/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      
+      username: loginUserAtual,
+      password: senhaUserAtual,
+      expiresInMins: 30, // optional, defaults to 60
+    })
+  })
+  .then(res => res.json())
+  .then(console.log);
 })
